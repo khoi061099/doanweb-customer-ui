6f72c06f3b9131b1767d2218d9ccaa1318918570
LL <liembsbs123@gmail.com>
2019-11-04 20:54:18 +0700

    create page cart, information, product-detail, search

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/product/:id", function(req, res, next) {
+  res.render("pages/product-detail");
+});
+
+router.get("/cart", function(req, res, next) {
+  res.render("pages/cart");
+});
+
+router.get("/search", function(req, res, next) {
+  res.render('pages/search')
 });
 
 module.exports = router;
